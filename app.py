352e86c370a155e57f0902f1234fa3c8df516122
aminhosseini7 <amin.h76@gmail.com>
2026-08-21 16:13:05 +0330

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("Renewable Energy PhD Funding Radar")
+
+st.write(
+'''
+Dashboard for monitoring funded PhD opportunities in:
+- Bioenergy
+- Biomass supply chains
+- Renewable energy optimization
+- Sustainable supply-chain network design
+'''
+)
+
+st.info("Connect this dashboard to the crawler database.")
---
+++ worker.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+print("Renewable Energy PhD Radar scan")
+print(datetime.now())
+print("Crawler placeholder. Add university adapters here.")
