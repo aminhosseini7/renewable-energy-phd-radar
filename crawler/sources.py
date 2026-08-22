ad478fd4d534925b69d362f8d7b6f68ab72fd85e
aminhosseini7 <amin.h76@gmail.com>
2026-08-23 00:24:12 +0330

    Delete crawler directory

--- crawler/sources.py
+++
@@ -1,8 +0,0 @@
-SOURCES=[
-{"name":"FindAPhD","url":"https://www.findaphd.com"},
-{"name":"EURAXESS","url":"https://euraxess.ec.europa.eu"},
-{"name":"University jobs","url":"https://www.jobs.ac.uk"}
-]
-
-def collect():
-    return []
--- crawler/university_adapter.py
+++
@@ -1,3 +0,0 @@
-def search_university(url):
-    # Framework for future university crawlers
-    return []
